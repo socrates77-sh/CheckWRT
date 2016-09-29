46cf803712107cb2f3cffb1179691a8d951ef07e
socrates77 <zhwenrong@sina.com>
2016-09-29 09:13:44 +0800

    update office env
    Signed-off-by: socrates77 <zhwenrong@sina.com>

--- CheckWRT/forMC30P6060.vb
+++ CheckWRT/forMC30P6060.vb
@@ -5,13 +5,14 @@ Imports System.Text
 Module forMC30P6060
     Private Const Product_Type_Name = "MC30P6060"                   '品名
     Private Const Option_Win_Title As String = "配置 MC30P6060"     '配置Option的窗口标题
+    Private Const Last_Option As Integer = 3                        '最后一个option编号
     Dim nIndexControl As Integer                                    '遍历控件的编号
-    Dim OptionFromWin(7) As UInt16                                  '存储Option窗口获取的option值
+    Dim OptionFromWin(Last_Option) As UInt16                                  '存储Option窗口获取的option值
 
     Public Function CheckAll() As Boolean
         Dim i As Integer
 
-        For i = 0 To 7
+        For i = 0 To Last_Option
             OptionFromWin(i) = &HFFFF
         Next
 
@@ -38,7 +39,7 @@ Module forMC30P6060
         Dim i As Integer
         'OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(3, 14, 3)
         myWriteLine("******************")
-        For i = 0 To 7
+        For i = 0 To Last_Option
             myWriteLine("OPTION[" & i.ToString & "]=" & OptionFromWin(i).ToString("X4"))
         Next
 #End If
@@ -87,91 +88,91 @@ Module forMC30P6060
         Select Case nIndexControl
             Case 7
                 If nResult = 1 Then
-                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(1, 13, 1)
+                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(1, 13, 1)    'RESSEL
                 End If
             Case 8
                 If nResult = 1 Then
-                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(0, 13, 1)
+                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(0, 13, 1)    'RESSEL
                 End If
             Case 10
                 If nResult = 1 Then
-                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(1, 5, 1)
+                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(1, 5, 1)     'SMTSEL
                 End If
             Case 11
                 If nResult = 1 Then
-                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(0, 5, 1)
+                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(0, 5, 1)     'SMTSEL
                 End If
             Case 13
                 If nResult = 1 Then
-                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(1, 4, 1)
+                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(1, 4, 1)     'MCUSEL
                 End If
             Case 14
                 If nResult = 1 Then
-                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(0, 4, 1)
+                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(0, 4, 1)     'MCUSEL
                 End If
             Case 16
                 If nResult = 1 Then
-                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(0, 3, 1)
+                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(0, 3, 1)     'SMTSEL
                 End If
             Case 17
                 If nResult = 1 Then
-                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(1, 3, 1)
+                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(1, 3, 1)     'SMTSEL
                 End If
             Case 19
                 If nResult = 1 Then
-                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(1, 2, 1)
+                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(1, 2, 1)     'RDPORT
                 End If
             Case 20
                 If nResult = 1 Then
-                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(0, 2, 1)
+                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(0, 2, 1)     'RDPORT
                 End If
             Case 22
                 If nResult = 1 Then
-                    OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(1, 13, 1)
+                    OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(1, 13, 1)    'CP
                 End If
             Case 23
                 If nResult = 1 Then
-                    OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(0, 13, 1)
+                    OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(0, 13, 1)    'CP
                 End If
             Case 25
                 If nResult = 1 Then
-                    OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(1, 8, 1)
+                    OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(1, 8, 1)     'FCPUS
                 End If
             Case 26
                 If nResult = 1 Then
-                    OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(0, 8, 1)
+                    OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(0, 8, 1)     'FCPUS
                 End If
             Case 30
                 If nResult = 1 Then
-                    OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(1, 4, 1)
+                    OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(1, 4, 1)     'MCLRE
                 End If
             Case 31
                 If nResult = 1 Then
-                    OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(0, 4, 1)
+                    OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(0, 4, 1)     'MCLRE
                 End If
             Case 33
                 If nResult = 1 Then
-                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(0, 1, 1)
+                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(0, 1, 1)     'IODRV
                 End If
             Case 34
                 If nResult = 1 Then
-                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(1, 1, 1)
+                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(1, 1, 1)     'IODRV
                 End If
             Case 40
                 If nResult = 1 Then
-                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(1, 7, 1)
+                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(1, 7, 1)     'ODSEL
                 End If
             Case 41
                 If nResult = 1 Then
-                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(0, 7, 1)
+                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(0, 7, 1)     'ODSEL
                 End If
             Case 43
                 If nResult = 1 Then
-                    OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(1, 12, 1)
+                    OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(1, 12, 1)    'WDTE
                 End If
             Case 46
                 If nResult = 1 Then
-                    OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(0, 12, 1)
+                    OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(0, 12, 1)    'WDTE
                 End If
         End Select
 
@@ -200,9 +201,69 @@ Module forMC30P6060
         '根据nIndexControl序号，分别处理对应option位
         Select Case nIndexControl
             Case 0
-                If nSelect = 1 Then
-                    OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(1, 13, 1)
-                End If
+                Select Case nSelect
+                    Case 0
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(0, 10, 3)    'OSCM
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(0, 6, 1)     'FILS
+                        OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(7, 7, 3)     'FINTOSC
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(1, 0, 1)     'ROTP
+                    Case 1
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(1, 10, 3)    'OSCM
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(0, 6, 1)     'FILS
+                        OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(7, 7, 3)     'FINTOSC
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(0, 0, 1)     'ROTP
+                    Case 2
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(2, 10, 3)    'OSCM
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(1, 6, 1)     'FILS
+                        OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(3, 7, 3)     'FINTOSC
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(1, 0, 1)     'ROTP
+                    Case 3
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(3, 10, 3)    'OSCM
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(1, 6, 1)     'FILS
+                        OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(7, 7, 3)     'FINTOSC
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(0, 0, 1)     'ROTP
+                    Case 4
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(5, 10, 3)    'OSCM
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(0, 6, 1)     'FILS
+                        OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(7, 7, 3)     'FINTOSC
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(0, 0, 1)     'ROTP
+                    Case 5
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(6, 10, 3)    'OSCM
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(1, 6, 1)     'FILS
+                        OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(7, 7, 3)     'FINTOSC
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(0, 0, 1)     'ROTP
+                    Case 6
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(6, 10, 3)    'OSCM
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(1, 6, 1)     'FILS
+                        OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(6, 7, 3)     'FINTOSC
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(0, 0, 1)     'ROTP
+                    Case 7
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(6, 10, 3)    'OSCM
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(1, 6, 1)     'FILS
+                        OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(5, 7, 3)     'FINTOSC
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(0, 0, 1)     'ROTP
+                    Case 8
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(6, 10, 3)    'OSCM
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(1, 6, 1)     'FILS
+                        OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(4, 7, 3)     'FINTOSC
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(0, 0, 1)     'ROTP
+                    Case 9
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(6, 10, 3)    'OSCM
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(1, 6, 1)     'FILS
+                        OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(2, 7, 3)     'FINTOSC
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(0, 0, 1)     'ROTP
+                    Case 10
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(6, 10, 3)    'OSCM
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(1, 6, 1)     'FILS
+                        OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(1, 7, 3)     'FINTOSC
+                        OptionFromWin(2) = OptionFromWin(2) And SetBitN_16(0, 0, 1)     'ROTP
+                End Select
+            Case 4
+                OptionFromWin(3) = OptionFromWin(3) And SetBitN_16(nSelect, 2, 3)     'FAS
+            Case 37
+                OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(nSelect, 3, 4)     'VLVRS
+            Case 44
+                OptionFromWin(0) = OptionFromWin(0) And SetBitN_16(7 - nSelect, 11, 3)    'TWDT
         End Select
     End Sub
 
@@ -215,13 +276,17 @@ Module forMC30P6060
         Try
             '逐级查找配置芯片Button的handle
             hWndMain = FindWindow(vbNullString, EZPRO_TITLE)
+            If hWndMain = 0 Then
+                MsgBox("EZpro100 not running!")
+                End
+            End If
             hGroupTool = FindWindowEx(hWndMain, 0, vbNullString, TOOL_GROUP_TEXT)
             hCmdConfig = FindWindowEx(hGroupTool, 0, vbNullString, CONFIG_BUTTON_TEXT)
 
             '点击配置芯片Button，打开Option窗口
             If Not hCmdConfig = 0 Then
                 PostMessage(hCmdConfig, BM_CLICK, 0, 0)
-                System.Threading.Thread.Sleep(300)
+                System.Threading.Thread.Sleep(300)          '等待300ms待窗口打开，否则可能取不到句柄
                 Return FindWindow(vbNullString, Option_Win_Title)
             Else
                 Return 0
