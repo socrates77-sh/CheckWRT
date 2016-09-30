24b30865d2128199a9bcf8097c0faeff24263200
socrates77 <zhwenrong@sina.com>
2016-09-30 22:49:08 +0800

    CheckWRT dev @home 2016/9/30
    Signed-off-by: socrates77 <zhwenrong@sina.com>

--- CheckWRT/Win32API.vb
+++ CheckWRT/Win32API.vb
@@ -2,18 +2,38 @@ Imports System.Text
 
 Public Module Win32API
 
-    'Button Control Messages
-    Public Const BM_CLICK = &HF5        'Button Click message
-    Public Const BM_GETCHECK = &HF0     'Radio button check
-    Public Const CB_GETCURSEL = &H147   'Combo box get selected item index
-    Public Const CB_GETCOUNT = &H146    'Combo box get count of items
-    Public Const CB_GETLBTEXT = &H148   'Combo box get selected item string
-    Public Const WM_GETTEXT = &HD       'Text box get text
-    Public Const WM_SYSCOMMAND = &H112
-    Public Const WM_COMMAND = &H111
-    Public Const WM_KEYDOWN = &H100
-    Public Const VK_RETURN = &HD
+    Public Const BM_CLICK As Integer = &HF5        'Button Click message
+    Public Const BM_GETCHECK As Integer = &HF0     'Radio button check
+    Public Const CB_GETCURSEL As Integer = &H147   'Combo box get selected item index
+    Public Const CB_GETCOUNT As Integer = &H146    'Combo box get count of items
+    Public Const CB_GETLBTEXT As Integer = &H148   'Combo box get selected item string
+    Public Const WM_GETTEXT As Integer = &HD       'Text box get text
+    Public Const WM_SYSCOMMAND As Integer = &H112
+    Public Const WM_COMMAND As Integer = &H111
+    Public Const WM_KEYDOWN As Integer = &H100
+    Public Const VK_RETURN As Integer = &HD
+    'TTreeView const
+    Public Const TV_FIRST As Integer = &H1100
+    Public Const TVM_SETBKCOLOR As Integer = TV_FIRST + 29
+    Public Const TVM_GETITEM As Integer = TV_FIRST + 12
+    Public Const TVIF_HANDLE As Integer = &H10
+    Public Const TVIS_SELECTED As Integer = &H2
+    Public Const TVIF_STATE As Integer = &H8
 
+
+
+    Public Structure TVITEM
+        Dim mask As Long
+        Dim HTreeItem As Integer
+        Dim state As Integer
+        Dim stateMask As Integer
+        Dim pszText As Integer
+        Dim cchTextMax As Integer
+        Dim iImage As Integer
+        Dim iSelectedImage As Integer
+        Dim cChildren As Integer
+        Dim lParam As Integer
+    End Structure
 
     Public Declare Auto Function FindWindow Lib "user32.dll" _
         Alias "FindWindow" (ByVal lpClassName As String, ByVal lpWindowName As String) As Integer
@@ -35,6 +55,9 @@ Public Module Win32API
 
     Public Declare Function SendMessageS Lib "user32.dll" _
         Alias "SendMessageA" (ByVal hwnd As Integer, ByVal wMsg As Integer, ByVal wParam As Integer, ByVal lParam As StringBuilder) As Integer
+
+    Public Declare Function SendMessageTV Lib "user32.dll" _
+       Alias "SendMessageA" (ByVal hwnd As Integer, ByVal wMsg As Integer, ByVal wParam As Integer, ByRef lParam As TVITEM) As Integer
 
     Public Declare Function PostMessage Lib "user32.dll" _
         Alias "PostMessageA" (ByVal hwnd As Integer, ByVal wMsg As Integer, ByVal wParam As Integer, ByVal lParam As Integer) As Integer
--- CheckWRT/myMain.vb
+++ CheckWRT/myMain.vb
@@ -9,41 +9,26 @@ Module myMain
 
     Dim sInfoText As String             '芯片信息栏的文本
     Dim alChipList As ArrayList         '所有芯片型号
-    Dim sEzproVersionFromFile As String         'EZPRO软件版本号，来自checklist文件
-    Dim sEzproVersionFromAbout As String         'EZPRO软件版本号，来自About窗口
+    'Dim sEzproVersionFromFile As String         'EZPRO软件版本号，来自checklist文件
+    'Dim sEzproVersionFromAbout As String         'EZPRO软件版本号，来自About窗口
+    Dim hTreeChipType As Integer        '芯片型号选择Tree的句柄
 
     Sub Main()
         '初始化
         InitMain()
 
+        GetInfoText()
+        myWriteLine(sInfoText)
+        GetChipTypeHandle()
+        myWriteLine(hTreeChipType)
         'forMC30P6060.CheckAll()
-
-        GetVersionFromAbout()
-        myWriteLine(sEzproVersionFromAbout)
-        'myWriteLine(hWndMain.ToString)
-        'ClickMenu(hWndMain, 4, 0)
-        'System.Threading.Thread.Sleep(1000)
-        'Dim hwnd As Integer = FindWindow(vbNullString, EZPRO_TITLE)
-        'myWriteLine(hwnd.ToString)
-        'PostMessage(hwnd, WM_KEYDOWN, VK_RETURN, 0)
-
-        'ExtractWindow()
-
-        ''myWriteLine(GetChipTypeHandle().ToString)
-
-        'myWriteLine(GetInfoText())
-        ''ExtractWindow()
-        'GetWindowInfoHandle()
-        'ClickMenu(hWndMain, 4, 0)
-        'System.Threading.Thread.Sleep(1000)
-        'Dim hwnd As Integer = FindWindow(hWndMain, EZPRO_TITLE)
-        'PostMessage(hWndMain, WM_KEYDOWN, VK_RETURN, 0)
 
         swLog.Close()
     End Sub
 
-    '获取配置选择芯片窗口的句柄
-    Private Function GetChipTypeHandle() As Integer
+    '获取配置选择芯片窗口的句柄利用EnumChildWindows及EnumWindowsCallback实现
+    '直接设置hTreeChipType变量
+    Private Sub GetChipTypeHandle()
         Try
             '逐级查找配置芯片Button的handle
             Dim hGroupTool As Integer = FindWindowEx(hWndMain, 0, vbNullString, TOOL_GROUP_TEXT)
@@ -54,51 +39,33 @@ Module myMain
                 PostMessage(hCmdConfig, BM_CLICK, 0, 0)
                 System.Threading.Thread.Sleep(300)          '等待300ms待窗口打开，否则可能取不到句柄
                 Dim hwndChip As Integer = FindWindow(vbNullString, CHIP_BUTTON_TEXT)
-                Dim hTreeChipType As Integer = FindWindowEx(hGroupTool, 0, "TTreeView", vbNullString)
-                Return hTreeChipType
+                myWriteLine(hwndChip)
+                'Dim hTreeChipType As Integer = FindWindowEx(hGroupTool, 0, "TTreeView", vbNullString)
+                'myWriteLine(hTreeChipType)
+                EnumChildWindows(hwndChip, New EnumWindowsCallback(AddressOf Do_FindTreeChipType), 0)
             Else
-                Return 0
             End If
         Catch ex As Exception
             MsgBox(ex.Message.ToString)
-            Return 0
         End Try
-    End Function
-
-    '取得芯片信息栏的文字，
-    '直接设置sEzproVersionFromAbout变量
-    Private Sub GetVersionFromAbout()
-        ClickMenu(hWndMain, 4, 0)
-        System.Threading.Thread.Sleep(3000)
-        '如果弹出无法找到串口的窗口，则关闭之
-        Dim hwndMessage As Integer = FindWindow(vbNullString, EZPRO_TITLE)
-        If hwndMessage Then
-            PostMessage(hwndMessage, WM_KEYDOWN, VK_RETURN, 0)
-            System.Threading.Thread.Sleep(5000)     '等待About窗口显示的时间，特别长！
-        End If
-        Dim hwndAbout = FindWindow(vbNullString, ABOUT_TITLE)
-        '利用EnumChildWindows及EnumWindowsCallback实现
-        EnumChildWindows(hwndAbout, New EnumWindowsCallback(AddressOf Do_FindVersionText), 0)
-        '关闭About窗口
-        System.Threading.Thread.Sleep(1000)
-        PostMessage(hwndAbout, WM_KEYDOWN, VK_RETURN, 0)
     End Sub
 
-    Private Function Do_FindVersionText(ByVal hWnd As Integer, ByVal lParam As Integer) As Boolean
-        Dim strControlText As New StringBuilder(STR_BUFFER_LEN)
-        'Dim strClassName As New StringBuilder(STR_BUFFER_LEN)
-        GetWindowText(hWnd, strControlText, STR_BUFFER_LEN)
-        'GetClassName(hWnd, strClassName, STR_BUFFER_LEN)
-
-        'myWriteLine("(Class)" & strClassName.ToString.PadRight(15) & "(Text)" & strControlText.ToString)
-        If strControlText.ToString.Contains("Version") Then             '查找到含Version那一个TEXT
-            sEzproVersionFromAbout = strControlText.ToString
+    Private Function Do_FindTreeChipType(ByVal hWnd As Integer, ByVal lParam As Integer) As Boolean
+        Dim strClassName As New StringBuilder(STR_BUFFER_LEN)
+        GetClassName(hWnd, strClassName, STR_BUFFER_LEN)
+        If strClassName.ToString = "TTreeView" Then             '唯一一个TTreeView控件即是芯片型号选择Tree
+            hTreeChipType = hWnd
+            SendMessage(hTreeChipType, TVM_SETBKCOLOR, 0, RGB(255, 0, 0))
+            'Dim tv As TVITEM
+            'tv.mask = TVIF_STATE Or TVIF_HANDLE
+            ''tv.state = TVIS_SELECTED
+            'SendMessageTV(hTreeChipType, TVM_GETITEM, 0, tv)
             Return False
         Else
             Return True
         End If
+        Return True
     End Function
-
 
     '取得芯片信息栏的文字，利用EnumChildWindows及EnumWindowsCallback实现
     '直接设置sInfoText变量
@@ -143,8 +110,6 @@ Module myMain
         '打开checklist文件，读取信息
         Try
             Dim sr As StreamReader = New StreamReader(CHKLIST_FILE_NAME)
-            '从checklist读EZPRO的版本
-            sEzproVersionFromFile = sr.ReadLine
 
             '从checklist读所有芯片型号
             alChipList = New ArrayList
